2813fe08f02d818c499ebb8083a63e9984b27dab
Казунин Константин <kostaykazunin@gmail.com>
2025-11-26 21:40:17 +0500

    модуль 9 задание 1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { generatePhotos } from './photo-generator.js';
 import { renderThumbnails } from './thumbnails.js';
+import './form.js';
 
 const photos = generatePhotos();
 export { photos };
