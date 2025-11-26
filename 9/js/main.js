ced23dedf21bac8aeed325e19be8638e18cb3396
Keks <keks@htmlacademy.ru>
2025-11-26 19:41:01 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/main.js
@@ -0,0 +1,14 @@
+import { generatePhotos } from './photo-generator.js';
+import { renderThumbnails } from './thumbnails.js';
+import './form.js';
+
+const photos = generatePhotos();
+export { photos };
+
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', () => {
+    renderThumbnails(photos);
+  });
+} else {
+  renderThumbnails(photos);
+}
