645c84ca11c20168430b2142df5040a0582b63da
Keks <keks@htmlacademy.ru>
2025-12-17 00:34:41 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/main.js
@@ -0,0 +1,18 @@
+import { generatePhotos } from './photo-generator.js';
+import { renderThumbnails } from './thumbnails.js';
+
+const photos = generatePhotos();
+export { photos };
+
+const initApp = () => {
+  renderThumbnails(photos);
+};
+
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', initApp);
+} else {
+  initApp();
+}
+
+// Загружаем форму после инициализации основного приложения
+import './form.js';
